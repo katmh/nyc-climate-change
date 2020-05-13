3e0d2338489f27d61a5f8d6d9cf8b498196727d4
Katherine Huang <hello@katmh.com>
2020-05-12 21:28:57 -0400

    Finishing touches

--- _posts/2020-05-10-conclusion.md
+++ _posts/2020-05-10-conclusion.md
@@ -2,6 +2,8 @@
 title: Conclusion
 ---
 
-So, what is the story here? While yes Local Law 97 is a critical law that needed to happen, it is also a law that puts New York City’s most vulnerable populations at risk. It appears Local Law 97’s costs, like many other aspects of climate change, will be disproportionately placed upon New York City’s poorest and most burdened populations. 
+While Local Law 97 is a critical, necessary law, it’s also a law that puts NYC’s most vulnerable populations at risk. It appears Local Law 97’s costs, like many other aspects of climate change, will be **disproportionately placed upon New York City’s poorest and most burdened populations**.
 
-Recognizing this, it is imperative that the city takes steps to secure an equitable outcome for Local Law 97. This may mean additional steps, or even laws, that guarantee the costs of the required energy improvements are not pushed onto tenants. Additionally, community engagement and renovation oversight agencies may also prove necessary. Community engagement, because it is critical that these communities know their rights going into this process. Oversight agencies, because no process is perfect, and it is essential that a means for recourse is established.
+Recognizing this, it is imperative that the city takes steps to secure an equitable outcome for Local Law 97. This may mean additional steps, or even laws, that guarantee the costs of the required energy improvements are not pushed onto tenants.
+
+Additionally, community engagement and renovation oversight agencies may also prove necessary. Community engagement, because it is critical that these communities know their rights going into this process. Oversight agencies, because no process is perfect, and it is essential that a means for recourse is established.
---
+++ _site/script.js
@@ -0,0 +1,132 @@
+mapboxgl.accessToken =
+  "pk.eyJ1IjoiY3JzaGF0ZmllbGQiLCJhIjoiY2s5bGZjYnhhMjVlcDNnbzRwcWlzbmU1ZSJ9.YQsNhcTftoNjzASR-38XaQ";
+
+var map = new mapboxgl.Map({
+  container: "map",
+  style: "mapbox://styles/crshatfield/cka1t4ujj14ir1joio9tr8xp3",
+  center: [-73.95, 40.71], // NYC: approximately 40 deg N, 74 deg W
+  zoom: 10,
+});
+
+var bldgClasses = ["allbuildings", "exempt-all", "exempt-res", "notexempt-res"],
+  demographicLayers = ["pctwhite", "education", "householdmedianincome", "totalrentburdened"];
+
+var labels = {
+  "pctwhite": ['<22%', '22%-41%', '41%-59%', '59%-75%', '75%<'],
+  "education": ['<42%', '42%-54%', '54%-67%', '67%-80%', '91%<'],
+  "householdmedianincome": ['<45.8k', '45.8k-70k', '70k-91k', '91k-111k', '111k<'],
+  "totalrentburdened": ['<41%', '41%-47%', '47%-53%', '53%-59%', '59%<']
+};
+
+var legend = document.querySelector("#legend");
+var menu = document.querySelector("#map-menu");
+var legendLabels = legend.querySelectorAll(".label");
+
+map.dragPan.disable();
+
+map.on("load", function() {
+  // initialize scrollama
+  var scroller = scrollama();
+
+  // set up stickyfill
+  d3.selectAll(".sticky").each(function () {
+    Stickyfill.add(this);
+  });
+
+  // force a resize on load to ensure proper dimensions are sent to scrollama
+  scroller.resize();
+
+  scroller
+    .setup({
+      step: ".step",
+      offset: 0.5,
+    })
+    .onStepEnter(response => {
+      // response = { element, direction, index }
+      currentClassList = response.element.classList;
+
+      bldgClasses.forEach(className => {
+        if (currentClassList.contains(className)) {
+          console.log(`${currentClassList} contains ${className}`)
+          map.setLayoutProperty(className, "visibility", "visible");
+        }
+      })
+
+      if (currentClassList.contains("demographics")) {
+        map.setLayoutProperty("householdmedianincome", "visibility", "visible");
+
+        // make legend and menu visible
+        legend.style.opacity = "1";
+        menu.style.opacity = "1";
+      }
+    })
+    .onStepExit(response => {
+      currentClassList = response.element.classList;
+
+      bldgClasses.forEach(className => {
+        if (currentClassList.contains(className) && !currentClassList.contains("demographics")) {
+          // if we're exiting the section right before the demographics,
+          // only hide exempt-res layer if we're going up
+          if (currentClassList.contains("before-demographics") && response.direction == "down") {
+            return;
+          }
+          map.setLayoutProperty(className, "visibility", "none");
+        }
+      })
+
+      // separate exit rule for demographics section
+      if (currentClassList.contains("demographics")) {
+        demographicLayers.forEach(layer => {
+          map.setLayoutProperty(layer, "visibility", "none");
+
+          // hide legend and menu
+          legend.style.opacity = "0";
+          menu.style.opacity = "0";
+        })
+      }
+    });
+
+  window.addEventListener("resize", scroller.resize);
+})
+
+// demographics menu
+let menuButtons = document.querySelector("#map-menu").getElementsByTagName("input");
+for (btn of menuButtons) {
+  btn.addEventListener("click", function(e) {
+    // "clear" button
+    if (e.target.id == "clear") {
+      // hide all demographic layers
+      for (layer of demographicLayers) {
+        map.setLayoutProperty(layer, "visibility", "none");
+      }
+      // remove checked style so it functions more like a button
+      e.target.checked = false;
+      // hide legend
+      legend.style.opacity = "0";
+      return;
+    }
+
+    // all other buttons
+    if (e.target.checked) {
+      // make selected layer visible
+      map.setLayoutProperty(e.target.id, "visibility", "visible");
+
+      // show legend if it's not visible
+      if (legend.style.opacity = "0") {
+        legend.style.opacity = "1";
+      }
+
+      // change legend labels
+      for (let i = 0; i < legendLabels.length; i++) {
+        legendLabels[i].innerText = labels[e.target.id][i];
+      }
+
+      // hide all other layers
+      for (layer of demographicLayers) {
+        if (layer != e.target.id) {
+          map.setLayoutProperty(layer, "visibility", "none");
+        }
+      }
+    }
+  })
+}
